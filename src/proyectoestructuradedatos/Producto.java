c67ffb7c6e70163f175579e353f867bf72b3c96d
Gabriel <gabrieluf2297@gmail.com>
2018-03-02 17:56:56 -0600

    Agregando clase producto

---
+++ src/proyectoestructuradedatos/Producto.java
@@ -0,0 +1,60 @@
+package proyectoestructuradedatos;
+
+/**
+ * Clase producto que contendra todo lo relevate a
+ * los productos del cliente
+ * @author gabriel
+ */
+public final class Producto {
+    private String nombre;
+    private int codigo, cantidadEnBodega, precioCompra, precioVenta;
+    
+    public Producto(String nombre, int codigo, int cantidadEnBodega, int precioCompra, int precioVenta){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.cantidadEnBodega = cantidadEnBodega;
+        this.precioCompra = precioCompra;
+        this.precioVenta = precioVenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCantidadEnBodega() {
+        return cantidadEnBodega;
+    }
+
+    public void setCantidadEnBodega(int cantidadEnBodega) {
+        this.cantidadEnBodega = cantidadEnBodega;
+    }
+
+    public int getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public void setPrecioCompra(int precioCompra) {
+        this.precioCompra = precioCompra;
+    }
+
+    public int getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(int precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+    
+}
